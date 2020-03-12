eff630b563cfc7c17c0f750a383e8b861baa1ae9
David Hambraeus <Ddhambraeus@gmail.com>
2020-03-12 18:11:45 +0100

    Cleanup and don't show 3d plot

--- 2d_wave_eq_square.py
+++ 2d_wave_eq_square.py
@@ -9,21 +9,24 @@ xmin = 0; xmax = 1; n = 100
 xs = np.linspace(xmin, xmax, n)
 ymin = 0; ymax = 1; n = 100
 ys = np.linspace(ymin, ymax, n)
+times = np.linspace(0,5,200)
 
 (X,Y) = np.meshgrid(xs,ys)
 
-ns = np.array(range(0,10))
-ms = np.array(range(0,11))
+ns = np.array(range(0,5))
+ms = np.array(range(0,5))
 
+# anms calculated from initial condition
+    # Sinusoidal anms: set anms zero and anms[j][k] = some constant
+    # to get initial condition f(x,y) = sin( pi j x) * sin( pi k y)
+    # These can be added by setting multiple anms to constants
 anms = np.zeros((len(ns),len(ms)))
-anms[2][2] = 1/4
+#anms[4][4] = 1/16
+#anms[2][3] = 1/4
+#anms[1][1] = 1
+anms[2][2] = 1/8
 
-def outer_along_first_axis(nxs, mys):
-    # Takes two arrays with u-values with different n values on 0th axis and different x/y values on 1st axis. 
-    # Returns ns on first axis, ms on second axis and the elementwise products of x and ys on third axis
-    return nxs[np.newaxis,:,:]*mys[:,np.newaxis,:]
 
-#ufcn = lambda t: np.sum(np.sum(anms[:,:,np.newaxis]*np.cos(np.pi*np.sqrt(np.add.outer(ns**2,ms**2))*t)[:,:,np.newaxis]* outer_along_first_axis(np.sin(np.pi*np.outer(ns,xs)),np.sin(np.pi*np.outer(ms,ys)))))
 def ufcn(t):
     Tnm = np.cos(np.pi*np.sqrt(np.add.outer(ns**2,ms**2))*t)
     subexp_1 = np.multiply(Tnm, anms)
@@ -32,31 +35,13 @@ def ufcn(t):
     XnsYms = np.multiply.outer(Xns,Yms)
     nsmsXsYs = np.swapaxes(XnsYms,1,2)
     fourDArr = subexp_1[:,:,np.newaxis,np.newaxis]*nsmsXsYs
-    #breakpoint()
     return np.sum(np.sum(fourDArr,axis=0),axis=0)
 
 
 u0 = ufcn(0)
 
-
-# Imshow figure
-
-fig = plt.figure(1)
-ax = fig.add_subplot(111)
-
 limit = max(np.abs(np.max(np.max(u0))),np.abs(np.min(np.min(u0))))
 norm = cm.colors.Normalize(vmax=limit, vmin=-limit)
-im = ax.imshow(u0,norm=norm)
-
-def animate(t):
-    us = ufcn(t)
-    im.set_data(us)
-    return im,
-
-times = np.linspace(0,2,100)
-
-#anim = FuncAnimation(fig,animate,init_func=None,frames = times, interval=10,blit=True,repeat=True, repeat_delay=0)
-
 
 # Surf plot figure
 
@@ -78,12 +63,32 @@ def animate(i):
     ax2.view_init(elev=20., azim=i)
     return surf,
 
-anim = FuncAnimation(fig2, animate, init_func=None, frames = len(times), interval=10, blit=False, repeat=True, repeat_delay=0)
+anim2 = FuncAnimation(fig2, animate, init_func=None, frames = len(times), interval=10, blit=False, repeat=True, repeat_delay=0)
 
 # Set up formatting for the movie files
 Writer = animation.writers['imagemagick']
 writer = Writer(fps=15, bitrate=1800)
 
-anim.save('test.gif',writer=writer)
+anim2.save('2dwave.gif',writer=writer)
+
+# Do not show this figure, it is rather slow, instead look at the gif
+plt.close()
+
+# Imshow figure
+
+fig = plt.figure(1)
+ax = fig.add_subplot(111)
+
+im = ax.imshow(u0,norm=norm)
+
+def animate(t):
+    us = ufcn(t)
+    im.set_data(us)
+    return im,
+
+
+anim = FuncAnimation(fig,animate,init_func=None,frames = times, interval=10,blit=True,repeat=True, repeat_delay=0)
+
+
 
 plt.show()
