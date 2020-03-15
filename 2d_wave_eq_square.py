f5db1ffec41d43319ee8ae21d2e95b2d2068533d
David Hambraeus <Ddhambraeus@gmail.com>
2020-03-15 15:26:00 +0100

    Added possibility of defining arbitrary functions as initial condition

--- 2d_wave_eq_square.py
+++ 2d_wave_eq_square.py
@@ -4,6 +4,7 @@ from matplotlib.animation import FuncAnimation
 import matplotlib.animation as animation
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
+import time
 
 xmin = 0; xmax = 1; n = 100
 xs = np.linspace(xmin, xmax, n)
@@ -13,19 +14,40 @@ times = np.linspace(0,5,200)
 
 (X,Y) = np.meshgrid(xs,ys)
 
-ns = np.array(range(0,5))
-ms = np.array(range(0,5))
+ns = np.array(range(0,20))
+ms = np.array(range(0,20))
 
 # anms calculated from initial condition
+anms = np.zeros((len(ns),len(ms)))
     # Sinusoidal anms: set anms zero and anms[j][k] = some constant
     # to get initial condition f(x,y) = sin( pi j x) * sin( pi k y)
     # These can be added by setting multiple anms to constants
-anms = np.zeros((len(ns),len(ms)))
 #anms[4][4] = 1/16
 #anms[2][3] = 1/4
 #anms[1][1] = 1
-anms[2][2] = 1/8
+#anms[2][2] = 1/8
 
+    # sin(pi x) * sin(4 pi x) * sin(pi y) * sin(4 pi y)
+#for n in ns:
+    #for m in ms:
+        #if n%2==0 and m%2==0:
+            #anms[n][m] = 16*n/(np.pi*(n**4 -34*n**2 + 225))* 16*m/(np.pi*(m**4 -34*m**2 + 225))
+
+
+
+f = lambda x,y: (x**2-x)*(y**2-y) 
+integralxs = np.linspace(0,1,100)
+integralys = np.linspace(0,1,100)
+anmfunc = lambda n, m: np.trapz(np.trapz(
+    f(integralxs, integralys) * np.outer(np.sin(n*np.pi*integralys),
+                                         np.sin(m*np.pi*integralys)),
+    dx =0.01),dx=0.01)
+
+for n in ns:
+    for m in ms:
+        anms[n][m] = anmfunc(n,m)
+
+print(f"Time taken for anms: {e-s}")
 
 def ufcn(t):
     Tnm = np.cos(np.pi*np.sqrt(np.add.outer(ns**2,ms**2))*t)
@@ -58,7 +80,7 @@ def animate(i):
     angle = dAngle*i
     us = ufcn(t)
     ax2.clear()
-    surf = ax2.plot_surface(X,Y,us,cmap='hot', norm=norm)
+    surf = ax2.plot_surface(X,Y,us,cmap='plasma', norm=norm)
     ax2.set_zlim([-limit,limit])
     ax2.view_init(elev=20., azim=i)
     return surf,
